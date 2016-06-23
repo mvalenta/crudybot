32aa07786e1d535c7b0d09ddd65fac54b58efc58
Mitch Valenta <mvalenta@live.com>
2016-06-23 14:01:27 -0400

    Added in EF and BotComms views

---
+++ CrudyBot/Controllers/BotCommsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using CrudyBot.Models;
+
+namespace CrudyBot.Controllers
+{
+    public class BotCommsController : Controller
+    {
+        private BotCommsContext db = new BotCommsContext();
+
+        // GET: BotComms
+        public ActionResult Index()
+        {
+            return View(db.BotComms.ToList());
+        }
+
+        // GET: BotComms/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            BotComm botComm = db.BotComms.Find(id);
+            if (botComm == null)
+            {
+                return HttpNotFound();
+            }
+            return View(botComm);
+        }
+
+        // GET: BotComms/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: BotComms/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,MessageText,ResponseText,MIMEType")] BotComm botComm)
+        {
+            if (ModelState.IsValid)
+            {
+                db.BotComms.Add(botComm);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(botComm);
+        }
+
+        // GET: BotComms/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            BotComm botComm = db.BotComms.Find(id);
+            if (botComm == null)
+            {
+                return HttpNotFound();
+            }
+            return View(botComm);
+        }
+
+        // POST: BotComms/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,MessageText,ResponseText,MIMEType")] BotComm botComm)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(botComm).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(botComm);
+        }
+
+        // GET: BotComms/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            BotComm botComm = db.BotComms.Find(id);
+            if (botComm == null)
+            {
+                return HttpNotFound();
+            }
+            return View(botComm);
+        }
+
+        // POST: BotComms/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            BotComm botComm = db.BotComms.Find(id);
+            db.BotComms.Remove(botComm);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ CrudyBot/Models/BotCommsContext.cs
@@ -0,0 +1,33 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace CrudyBot.Models
+{
+    using System;
+    using System.Data.Entity;
+    using System.Linq;
+
+    public class BotCommsContext : DbContext
+    {
+        // Your context has been configured to use a 'BotCommsContext' connection string from your application's 
+        // configuration file (App.config or Web.config). By default, this connection string targets the 
+        // 'CrudyBot.Models.BotCommsContext' database on your LocalDb instance. 
+        // 
+        // If you wish to target a different database and/or database provider, modify the 'BotCommsContext' 
+        // connection string in the application configuration file.
+        public BotCommsContext()
+            : base("name=BotCommsContext")
+        {
+        }
+
+        public virtual DbSet<BotComm> BotComms { get; set; }
+    }
+
+    public class BotComm
+    {
+        public int Id { get; set; }
+        public string MessageText { get; set; }
+        public string ResponseText { get; set; }
+        [StringLength(255)]
+        public string MIMEType { get; set; }
+    }
+}
